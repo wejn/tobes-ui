a4982bc22084ad484cb6c6c5d9a5d92b36039672
Michal Jirků <box@wejn.org>
2026-09-19 19:55:15 +0200

    OO WLC: Factor out CommonGUI
    Internal refactor, to allow reuse of some of the code.

---
+++ tobes_ui/common_gui.py
@@ -0,0 +1,131 @@
+"""Common GUI class, taking away commonalities."""
+
+from abc import abstractmethod
+import queue
+import pprint # pylint: disable=unused-import
+import threading
+import time
+
+
+from tobes_ui.common import AttrDict
+from tobes_ui.logger import LOGGER # pylint: disable=unused-import
+from tobes_ui.types import RefreshType
+
+
+class CommonGUI:  # pylint: disable=too-few-public-methods
+    """Common GUI window with a spectrometer instance."""
+
+    def __init__(self, root, spectrometer, title="Untitled UI window"):
+        self._root = root
+        self._root.title(title)
+        self._root.protocol("WM_DELETE_WINDOW", self._on_close)
+        self._root.geometry("1200x800")
+        self._root.minsize(1200, 800)
+
+        self._spectrometer = spectrometer
+        self._refresh_type = RefreshType.NONE if spectrometer else RefreshType.DISABLED
+
+        self._event_queue = queue.Queue()  # TK events submitted from non-main thread
+        self._worker_thread = threading.Thread(target=self._data_refresh_loop, daemon=True)
+        self._worker_thread.start()
+
+        self._ui_elements = AttrDict()  # all the different UI elements we need access to
+
+        self._setup_ui()
+
+        # Kick off event Q processing...
+        self._root.after(0, self._process_event_queue)
+
+        self._update_status('Ready.')
+
+    def _process_event_queue(self):
+        while not self._event_queue.empty():
+            event = self._event_queue.get_nowait()
+            event()
+        if self._refresh_type in [RefreshType.ONESHOT, RefreshType.CONTINUOUS]:
+            # Make queue processing more snappy when capturing...
+            self._root.after(20, self._process_event_queue)
+        else:
+            self._root.after(100, self._process_event_queue)
+
+    def _push_event(self, event):
+        if callable(event):
+            self._event_queue.put(event)
+        else:
+            raise ValueError(f"Event {event} is not callable")
+
+    @abstractmethod
+    def _setup_ui(self):
+        """Setup UI, inserting elements into self._ui_elements as needed."""
+
+    def _set_refresh_type(self, rt):
+        match rt:
+            case RefreshType.CONTINUOUS:
+                self._update_status('Starting capture...')
+
+            case RefreshType.NONE:
+                self._update_status('Stopping capture...')
+
+            case RefreshType.ONESHOT:
+                self._update_status('Starting single capture...')
+
+            case RefreshType.DISABLED:
+                self._update_status('Disabling capture...')
+
+            case _:
+                pass # Ignore
+
+        self._refresh_type = rt
+
+    @abstractmethod
+    def _process_spectrum(self, spectrum):
+        """Processes captured spectrum, runs in main thread"""
+
+    def _data_refresh_loop(self):
+        # WARNING: Does NOT run in main thread; do not run any Tkinter code here!
+        while True:
+            match self._refresh_type:
+                case RefreshType.DISABLED:
+                    return
+
+                case RefreshType.NONE:
+                    time.sleep(0.1)
+
+                case RefreshType.ONESHOT | RefreshType.CONTINUOUS:
+                    def handle_spectrum(value):
+                        #LOGGER.debug("Got spectrum data with %s status and %.2f integration",
+                        #             value.status, value.time)
+                        # FIXME: test if it is "good"...
+                        self._push_event(lambda: self._process_spectrum(value))
+                        if self._refresh_type in [RefreshType.NONE, RefreshType.ONESHOT]:
+                            self._refresh_type = RefreshType.NONE
+                            self._push_event(lambda: self._update_status('Capture stopped.'))
+                            self._push_event(self._on_capture_stop)
+                        return self._refresh_type == RefreshType.CONTINUOUS
+                    self._push_event(lambda: self._update_status('Capture running...'))
+                    self._spectrometer.stream_data(handle_spectrum)
+
+    @abstractmethod
+    def _on_capture_stop(self):
+        """Event that runs when capture ist stopped in refresh loop."""
+
+    def _update_status(self, message):
+        if 'status_label' in self._ui_elements:
+            self._ui_elements.status_label.config(text=message)
+        else:
+            print('Status:', message)
+
+    def _on_close(self):
+        self._update_status('Terminating capture...')
+        self._refresh_type = RefreshType.DISABLED
+        if self._worker_thread:
+            self._worker_thread.join()
+            self._worker_thread = None
+
+        self._update_status('Terminating spectrometer...')
+        if self._spectrometer:
+            self._spectrometer.cleanup()
+            self._spectrometer = None
+
+        self._update_status('Bye!')
+        self._root.destroy()
--- tobes_ui/wavelength_calibration.py
+++ tobes_ui/wavelength_calibration.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """Calibration for Ocean Optics spectrometer"""
 
-import queue
 import pprint # pylint: disable=unused-import
 import sys
-import threading
-import time
 import tkinter as tk
 from tkinter import ttk, messagebox
 
@@ -28,39 +25,26 @@ from tobes_ui.calibration.wavelength_save_dialog import WavelengthCalibrationSav
 from tobes_ui.calibration.x_axis_control import XAxisControl
 from tobes_ui.calibration.x_axis_zoom_control import XAxisZoomControl
 from tobes_ui.common import AttrDict, SlidingMax, SpectrumAggregator
+from tobes_ui.common_gui import CommonGUI
 from tobes_ui.logger import LogLevel, configure_logging, LOGGER, set_level
 from tobes_ui.spectrometer import ExposureMode, Spectrometer
 from tobes_ui.strong_lines_container import StrongLinesContainer
 from tobes_ui.types import RefreshType
 
 
-class WavelengthCalibrationGUI: # pylint: disable=too-few-public-methods
+class WavelengthCalibrationGUI(CommonGUI): # pylint: disable=too-few-public-methods
     """GUI for Ocean spectrometer wavelength calibration."""
 
     def __init__(self, root, spectrometer):
         if not spectrometer.supports_wavelength_calibration():
             raise ValueError("Spectrometer doesn't support WL calibration.")
 
-        self._root = root
-        self._root.title("Wavelength Calibration")
-        self._root.protocol("WM_DELETE_WINDOW", self._on_close)
-        self._root.geometry("1200x800")
-        self._root.minsize(1200, 800)
-
-        self._spectrometer = spectrometer
-        self._refresh_type = RefreshType.NONE  # initial
-        self._event_queue = queue.Queue()  # TK events submitted from non-main thread
-        self._worker_thread = threading.Thread(target=self._data_refresh_loop, daemon=True)
-        self._worker_thread.start()
-
-        initial_polyfit = self._spectrometer.read_wavelength_calibration()
+        initial_polyfit = spectrometer.read_wavelength_calibration()
         self._initial_polyfit = np.array(initial_polyfit)  # Current pixel -> wavelength polynomial
         self._new_polyfit = None  # New (calibrated) pixel -> wavelength polynomial
         self._new_polyfit_stats = None  # New polyfit stats
         self._x_axis_type = None  # Type of x axis coords (initial, fixed, new)
         self._x_axis_idx = None  # polyfit for the x axis (index for each pixel)
-
-        self._ui_elements = AttrDict()  # all the different UI elements we need access to
 
         self._spectrum_agg = SpectrumAggregator(1)
         self._spectrum = None  # Spectrum captured by spectrometer (last)
@@ -75,30 +59,7 @@ class WavelengthCalibrationGUI: # pylint: disable=too-few-public-methods
 
         self._annot_lims = None  # (xlim, ylim) for which annot was set up
 
-        self._setup_ui()
-
-        # Kick off event Q processing...
-        self._root.after(0, self._process_event_queue)
-
-        self._set_refresh_type(RefreshType.NONE)  # paused
-
-        self._update_status('Ready.')
-
-    def _process_event_queue(self):
-        while not self._event_queue.empty():
-            event = self._event_queue.get_nowait()
-            event()
-        if self._refresh_type in [RefreshType.ONESHOT, RefreshType.CONTINUOUS]:
-            # Make queue processing more snappy when capturing...
-            self._root.after(20, self._process_event_queue)
-        else:
-            self._root.after(100, self._process_event_queue)
-
-    def _push_event(self, event):
-        if callable(event):
-            self._event_queue.put(event)
-        else:
-            raise ValueError(f"Event {event} is not callable")
+        super().__init__(root, spectrometer, "Wavelength Calibration")
 
     def _setup_ui(self):
         self._root.grid_rowconfigure(0, weight=1)
@@ -427,25 +388,6 @@ class WavelengthCalibrationGUI: # pylint: disable=too-few-public-methods
                 LOGGER.debug("unhandled state: %s", self._refresh_type)
                 self._update_status(f'unhandled capture state: {self._refresh_type}')
 
-    def _set_refresh_type(self, rt):
-        match rt:
-            case RefreshType.CONTINUOUS:
-                self._update_status('Starting capture...')
-
-            case RefreshType.NONE:
-                self._update_status('Stopping capture...')
-
-            case RefreshType.ONESHOT:
-                self._update_status('Starting single capture...')
-
-            case RefreshType.DISABLED:
-                self._update_status('Disabling capture...')
-
-            case _:
-                pass # Ignore
-
-        self._refresh_type = rt
-
     def _process_spectrum(self, spectrum):
         """Processes captured spectrum"""
         if 'integration_control' in self._ui_elements:
@@ -571,30 +513,6 @@ class WavelengthCalibrationGUI: # pylint: disable=too-few-public-methods
         self._peaks = list(self._peak_detector(self._spectrum.spd_raw))
         LOGGER.debug("Detected %d peaks", len(self._peaks))
         self._update_plot(peaks=True)
-
-    def _data_refresh_loop(self):
-        # WARNING: Does NOT run in main thread; do not run any Tkinter code here!
-        while True:
-            match self._refresh_type:
-                case RefreshType.DISABLED:
-                    return
-
-                case RefreshType.NONE:
-                    time.sleep(0.1)
-
-                case RefreshType.ONESHOT | RefreshType.CONTINUOUS:
-                    def handle_spectrum(value):
-                        #LOGGER.debug("Got spectrum data with %s status and %.2f integration",
-                        #             value.status, value.time)
-                        # FIXME: test if it is "good"...
-                        self._push_event(lambda: self._process_spectrum(value))
-                        if self._refresh_type in [RefreshType.NONE, RefreshType.ONESHOT]:
-                            self._refresh_type = RefreshType.NONE
-                            self._push_event(lambda: self._update_status('Capture stopped.'))
-                            self._push_event(self._on_capture_stop)
-                        return self._refresh_type == RefreshType.CONTINUOUS
-                    self._push_event(lambda: self._update_status('Capture running...'))
-                    self._spectrometer.stream_data(handle_spectrum)
 
     def _on_capture_stop(self):
         """Event that runs when capture ist stopped in refresh loop."""
@@ -1017,27 +935,6 @@ class WavelengthCalibrationGUI: # pylint: disable=too-few-public-methods
         if redraw:
             #LOGGER.debug('redraw: %s', nearest_x)
             canvas.draw_idle()
-
-    def _update_status(self, message):
-        if 'status_label' in self._ui_elements:
-            self._ui_elements.status_label.config(text=message)
-        else:
-            print('Status:', message)
-
-    def _on_close(self):
-        self._update_status('Terminating capture...')
-        self._refresh_type = RefreshType.DISABLED
-        if self._worker_thread:
-            self._worker_thread.join()
-            self._worker_thread = None
-
-        self._update_status('Terminating spectrometer...')
-        if self._spectrometer:
-            self._spectrometer.cleanup()
-            self._spectrometer = None
-
-        self._update_status('Bye!')
-        self._root.destroy()
 
 if __name__ == "__main__":
     def main():
